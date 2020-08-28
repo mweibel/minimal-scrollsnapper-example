37ec3b5e8b354851c4eb2a8d786ddd6fdf901c41
mweibel <mweibel@users.noreply.github.com>
2020-08-28 15:23:50 +0000

    deploy: 05c66b2289e8e6d8b442d062dc42309bfa21df80

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d9f1b0614ae692bb64580ae0c679a8e.js"
+  "/minimal-scrollsnapper-example/precache-manifest.22bbe5a7d86afe47bb06e5c3063bff80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minimal-scrollsnapper-example/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
