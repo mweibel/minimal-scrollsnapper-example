6d3acc9f1f958b0c4e38bd16ef61bce4a703b9c7
Michael Weibel <michael.weibel@gmail.com>
2020-08-28 17:06:04 +0200

    minimal scroll snap accordion issue example

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import React from "react";
+import cx from "classnames";
+import {
+  CssBaseline,
+  makeStyles,
+  Grid,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@material-ui/core";
+import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  "@global": {
+    body: {
+      "overscroll-behavior-y": "none",
+    },
+
+    // FF/Chrome need it on html, Safari needs it on body
+    "html,body": {
+      scrollSnapType: "y mandatory",
+    },
+  },
+  header: {
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: " 100px",
+    zIndex: 3000,
+    position: "fixed",
+    background: "white",
+  },
+  page: {
+    marginTop: "100px",
+  },
+  section: {
+    height: "calc(100vh - 100px)",
+    scrollSnapAlign: "end",
+    display: "flex",
+  },
+  sectionContent: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    boxSizing: "border-box",
+  },
+  firstSection: {
+    background: "red",
+  },
+});
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <div className="App">
+      <CssBaseline />
+      <div className={classes.header}>header</div>
+      <div className={classes.page}>
+        <div className={cx(classes.section, classes.firstSection)}>
+          first section
+        </div>
+        <div className={classes.section}>
+          <Grid container className={classes.sectionContent}>
+            <Grid item md={6}>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                >
+                  <Typography className={classes.heading}>
+                    Accordion 1
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel2a-content"
+                  id="panel2a-header"
+                >
+                  <Typography className={classes.heading}>
+                    Accordion 2
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel3a-content"
+                  id="panel3a-header"
+                >
+                  <Typography className={classes.heading}>
+                    Accordion 3
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel4a-content"
+                  id="panel4a-header"
+                >
+                  <Typography className={classes.heading}>
+                    Accordion 4
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
